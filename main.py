82153aa13ddca89d9b79333f03a6770c689c4141
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-17 15:54:28 +0330

    a
    a

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
+
 pygame.init()
+
+screen = pygame.display.set_mode((400,400))
+
 class Dog:
     def __init__(self, name, age, gender, sound, image):
         self.name = name
@@ -9,8 +13,18 @@ class Dog:
         self.image = image
 
     def bark(self):
+        self.sound.play()
         print(f"{self.name} is Barking")
 
 
-dog1 = Dog("jessi", 6, "girl", pygame.mixer.Sound("w1.wav"), pygame.image.load("w1.png"))
+dog1 = Dog("jessi", 6, "girl", pygame.mixer.Sound("w2.mp3"), pygame.image.load("w1.png"))
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                dog1.bark()
