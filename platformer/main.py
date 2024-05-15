d30aa80961847707c477fe405dbb8d4454117e0f
msa-sadeghi <sadeghi.msai@gmail.com>
2024-05-15 15:51:56 +0330

    a

---
+++ platformer/main.py
@@ -0,0 +1,13 @@
+import pygame
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+FPS = 60
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+    clock.tick(FPS)
---
+++ review/1.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+def login():
+    if user.get() == "amir" and passw.get() == "123":
+        r.set(f"welcome {user.get()}")
+    else:
+        exit()      
+root = tk.Tk()
+user = tk.StringVar()
+passw = tk.StringVar()
+r = tk.StringVar()
+first_frame = tk.Frame(root)
+first_frame.pack()
+user_label = tk.Label(first_frame, text="username")
+user_label.pack(side="left")
+user_text_box = tk.Entry(first_frame, textvariable=user)
+user_text_box.pack(side="left")
+second_frame = tk.Frame(root)
+second_frame.pack()
+pass_label = tk.Label(second_frame, text="password")
+pass_label.pack(side="left")
+pass_text_box = tk.Entry(second_frame, textvariable=passw)
+pass_text_box.pack(side="left")
+submit_btn = tk.Button(root, text="login", command=login)
+submit_btn.pack()
+result = tk.Label(root, textvariable=r)
+result.pack()
+root.mainloop()
